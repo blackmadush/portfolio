22a87b88d7e95c430f10eb1c9ace2569e47d1caf
Madusha Bandara <madusha.bandara@ogilvy.com>
2024-07-20 22:30:35 +0530

    Add deployment setup

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/web/", // Set base path to your repository name
+  base: "/portfolio/", // Replace 'web' with your repository name
   plugins: [react()],
 });
